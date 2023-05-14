7d4b7fec3f701f8f3f1a0361becc7735f0d55b34
DanielCodingRaven <103775773+DanielProgrammer64@users.noreply.github.com>
2023-05-14 03:24:19 -0300

    Componentes do Projeto
    ///////////

---
+++ HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function HomeScreen() {
+  return (
+    <View>
+      <Text>Tela de inicio feita por Daniel Assis Programador 💎</Text>
+    </View>
+  );
+}
+
+export default HomeScreen;
